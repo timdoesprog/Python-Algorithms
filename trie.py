5de86c33d7fd234420aee1170f992a6c49c82d3b
Tim Betz <writingtumblr@gmx.de>
2017-06-04 13:35:49 +0200

    Changed Trie.py
    Made it use the dictionary correctly and made the code a little easier
    to read.

--- trie.py
+++ trie.py
@@ -23,17 +23,12 @@ class Trie:
     def insert(self, word):
         word = word.lower()
         parent = self.root
-        while word:
-            for char in parent.children:
-                if char == word[0]:
-                    parent = parent.children[char]
-                    word = word[1:]
-                    break
+        for letter in word:
+            if letter in parent.children:
+                parent = parent.children[letter]
             else:
-                new = Node(word[0])
-                parent.children[new.data] = new
-                parent = new
-                word = word[1:]
+                parent.children[letter] = Node(letter)
+                parent = parent.children[letter]
         parent.isEnd = True
 
     def insertAll(self, word_list):
@@ -43,10 +38,9 @@ class Trie:
     def contains(self, word):
         word = word.lower()
         parent = self.root
-        while word:
-            if word[0] in parent.children:
-                parent = parent.children[word[0]]
-                word = word[1:]
+        for letter in word:
+            if letter in parent.children:
+                parent = parent.children[letter]
             else:
                 return False
         return parent.isEnd
